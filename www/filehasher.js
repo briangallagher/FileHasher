32505dc1ac49aaabeb332e116bf5adb6d987a085
Brian Gallagher <brian.gallagher@feedhenry.com>
2016-02-15 16:05:49 +0000

    update js

--- www/filehasher.js
+++ www/filehasher.js
@@ -1,12 +1,14 @@
+var argscheck = require('cordova/argscheck'),
+  exec = require('cordova/exec');
+
 var FileHasher = function () {}
 
 FileHasher.prototype.hash = function (path, digestType, cb) {
-  debugger;
   if (typeof digestType === 'function') {
     cb = digestType;
     digestType = 'MD5';
   }
-  cordova.exec(function (hash) {
+  exec(function (hash) {
     return cb(null, hash);
   }, function (err) {
     return cb(new Error(err));
